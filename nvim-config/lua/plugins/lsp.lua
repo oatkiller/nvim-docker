98e6b47f51f20e3eee1d6cc460ed37f8f183e3ee
Robert Austin <robert@prequel.dev>
2025-05-28 09:48:46 -0400

    working enough

--- nvim-config/lua/plugins/lsp.lua
+++ nvim-config/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 vim.lsp.config('typescript',{
   cmd = { 'typescript-language-server', '--stdio' },
   filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript', 'javascriptreact', 'javascript.jsx' },
-  -- root_markers = { { 'tsconfig.json', 'jsconfig.json', 'package.json' }, '.git' },
+  root_markers = { 'tsconfig.json' },
 })
 
 vim.lsp.enable('typescript') 
