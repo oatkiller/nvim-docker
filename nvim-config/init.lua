d9406831e3ed8422e201f93c79a39eb03d635793
Robert Austin <robert@prequel.dev>
2025-06-04 09:01:59 -0400

    tweak folding settings

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -7,7 +7,11 @@ vim.opt.grepprg = "rg --vimgrep --smart-case"
 vim.opt.grepformat = "%f:%l:%c:%m"
 
 
+-- Use treesitter folding by default
 vim.o.foldmethod = 'expr'
+-- Open all folds by default
+vim.o.foldlevel = 99
+
 -- Default to treesitter folding
 vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 -- Prefer LSP folding if client supports it
@@ -16,6 +20,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client:supports_method('textDocument/foldingRange') then
       local win = vim.api.nvim_get_current_win()
+      -- Prefer LSP folding if client supports it
       vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
     end
   end,
