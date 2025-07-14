22448028eead45aabf60a516285dbed8d496e790
Robert Austin <robert@prequel.dev>
2025-07-14 13:26:28 -0400

    prevent eslintd from running in most projects

--- pack/none-ls-config/start/none-ls-config/plugin/none_ls.lua
+++ pack/none-ls-config/start/none-ls-config/plugin/none_ls.lua
@@ -2,7 +2,48 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    require("none-ls.diagnostics.eslint_d"),
+    -- Conditionally enable eslint_d only if the project has an ESLint configuration
+    require("none-ls.diagnostics.eslint_d").with({
+      condition = function(utils)
+        -- Enable if standard ESLint config files exist
+        if utils.root_has_file({
+          ".eslintrc",
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+          ".eslintrc.json",
+        }) then
+          return true
+        end
+
+        -- Fall-back: inspect package.json for eslint hints
+        local pkg_path = utils.root_has_file("package.json")
+        if not pkg_path then
+          return false
+        end
+
+        local ok, pkg = pcall(vim.fn.json_decode, table.concat(vim.fn.readfile(pkg_path), "\n"))
+        if not ok or type(pkg) ~= "table" then
+          return false
+        end
+
+        if pkg.eslintConfig then
+          return true
+        end
+
+        local function has_dep(tbl, name)
+          return tbl and tbl[name] ~= nil
+        end
+
+        if has_dep(pkg.dependencies, "eslint")
+          or has_dep(pkg.devDependencies, "eslint")
+          or has_dep(pkg.peerDependencies, "eslint")
+        then
+          return true
+        end
+
+        return false
+      end,
+    }),
     null_ls.builtins.formatting.prettier,
   },
   on_attach = function(client, bufnr)
