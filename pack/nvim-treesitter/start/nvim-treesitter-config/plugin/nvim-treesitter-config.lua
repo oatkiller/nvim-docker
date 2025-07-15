6932575d830db7c66de4fdde7aa1045663b7e166
Robert Austin <robert@prequel.dev>
2025-07-15 10:14:01 -0400

    fix(treesitter): ensure 'diff' parser is installed for better diff highlighting (task 2 complete)

--- pack/nvim-treesitter/start/nvim-treesitter-config/plugin/nvim-treesitter-config.lua
+++ pack/nvim-treesitter/start/nvim-treesitter-config/plugin/nvim-treesitter-config.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "typescript", "tsx" },
+  ensure_installed = { "typescript", "tsx", "diff" },
   auto_install = true,
   highlight = {
     enable = true,
